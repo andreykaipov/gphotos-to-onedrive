efe3e7d28c87539ef4183273092993fe26efbbe9
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-07-22 23:45:17 -0400

    convenient copy script

---
+++ config/copy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# shellcheck disable=SC2086
+
+: "${start=2005}"
+: "${end=$(date +%Y)}"
+
+mv rclone.log rclone.log.old
+
+flags="--progress --log-file=rclone.log"
+if [ -z "$NO_DRY_RUN" ]; then
+        flags="$flags --dry-run"
+fi
+
+for y in $(seq $start "$end"); do
+        for m in $(seq -w 1 12); do
+                echo ">>> $y $m <<<"
+                source="gphotos:/media/by-month/$y/$y-$m"
+                remote="onedrive:/media/albums/$y/$m"
+                rclone copy $flags "$source" "$remote"
+                echo
+        done
+done
