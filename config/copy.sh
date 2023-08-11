4f18ac5643f2fc4da85a42e170835af15037b1db
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2023-08-11 19:03:46 -0400

    copy.sh: abstract destination root

--- config/copy.sh
+++ config/copy.sh
@@ -4,8 +4,9 @@
 : "${start=2000}"
 : "${end=$(date +%Y)}"
 : "${LOG_LEVEL=INFO}"
+: "${DESTINATION_ROOT?}"
 
-mv rclone.log rclone.log.old
+mv rclone.log rclone.log.old || :
 
 flags="--progress --log-file=rclone.log --log-level=$LOG_LEVEL"
 if [ -z "$NO_DRY_RUN" ]; then
@@ -16,7 +17,7 @@ for y in $(seq $start "$end"); do
         for m in $(seq -w 1 12); do
                 echo ">>> $y $m <<<"
                 source="gphotos:/media/by-month/$y/$y-$m"
-                remote="onedrive:/media/albums/$y/$m"
+                remote="onedrive:$DESTINATION_ROOT/$y/$m"
                 rclone copy $flags "$source" "$remote"
                 echo
         done
