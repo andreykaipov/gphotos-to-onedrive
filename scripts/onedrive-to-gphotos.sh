ca230ec0d05ecd0df94069e85e4332d305db99a7
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2025-05-08 20:56:41 -0400

    afads

--- scripts/onedrive-to-gphotos.sh
+++ scripts/onedrive-to-gphotos.sh
@@ -1,5 +1,5 @@
 : "${start=2001}"
-: "${end=2009}"
+: "${end=$(date +%Y)}"
 
 flags='--config /tmp/rclone.conf --no-check-certificate --log-level INFO'
 
@@ -8,11 +8,11 @@ if [ -z "$NO_DRY_RUN" ]; then
 fi
 
 for y in $(seq $start "$end"); do
-        for m in $(seq -w 1 12); do
-                echo ">>> $y $m <<<"
-                source="onedrive:media/albums/$y/$m"
-                remote="gphotos:upload"
-                rclone $flags copy "$source" $remote
-                echo
-        done
+        # for m in $(seq -w 1 12); do
+        echo ">>> $y $m <<<"
+        source="onedrive:media/albums/$y"
+        remote="gphotos:upload"
+        rclone $flags copy "$source" $remote --metadata
+        echo
+        # done
 done
