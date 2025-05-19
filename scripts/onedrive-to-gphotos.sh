060ed4a5d4e615a0fbbe5632e38aa69aa828605f
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2025-05-19 10:40:46 -0400

    m

--- scripts/onedrive-to-gphotos.sh
+++ scripts/onedrive-to-gphotos.sh
@@ -7,17 +7,17 @@ if [ -z "$NO_DRY_RUN" ]; then
         flags="$flags --dry-run"
 fi
 
-source="onedrive:media/albums/$y"
-remote="gphotos:upload"
-rclone $flags move "$source" $remote --metadata --no-update-modtime --order-by modtime --no-check-dest --retries 1 --checksum
+# source="onedrive:media/albums/$y"
+# remote="gphotos:upload"
+# rclone $flags move "$source" $remote --metadata --no-update-modtime --order-by modtime --no-check-dest --retries 1 --checksum
 
-# for y in $(seq $start "$end"); do
-#         # for m in $(seq -w 1 12); do
-#         echo ">>> $y $m <<<"
-#         source="onedrive:media/albums/$y"
-#         remote="gphotos:upload"
-#         rclone $flags move "$source" $remote --metadata --no-update-modtime --order-by modtime --no-check-dest --retries 1 --checksum
-#         # --no-traverse
-#         echo
-#         # done
-# done
+for y in $(seq $start "$end"); do
+        # for m in $(seq -w 1 12); do
+        echo ">>> $y $m <<<"
+        source="onedrive:media/albums/$y"
+        remote="gphotos:upload"
+        rclone $flags move "$source" $remote --metadata --no-update-modtime --order-by modtime --no-check-dest --retries 1 --checksum
+        # --no-traverse
+        echo
+        # done
+done
