6a94a865b1ea54ea2d9ecee6618c4b3d61683985
Andrey Kaipov <andreykaipov@users.noreply.github.com>
2025-05-19 10:42:41 -0400

    m

--- scripts/onedrive-to-gphotos.sh
+++ scripts/onedrive-to-gphotos.sh
@@ -14,7 +14,7 @@ fi
 for y in $(seq $start "$end"); do
         # for m in $(seq -w 1 12); do
         echo ">>> $y $m <<<"
-        source="onedrive:media/albums/$y"
+        source="onedrive:media/mom's photos/$y"
         remote="gphotos:upload"
         rclone $flags move "$source" $remote --metadata --no-update-modtime --order-by modtime --no-check-dest --retries 1 --checksum
         # --no-traverse
